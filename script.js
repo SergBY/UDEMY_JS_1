aca5ff35b658709172b497a91e0ece69a2748d73
Sergey Ivanchik <sergeyivanchik@gmail.com>
2020-05-16 18:02:30 +0300

    create HW1_movies

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-'use strict';
+"use strict";
+
+let numberOfFilms = +prompt ('Сколько фидьмов вы уже посмотрели?');
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+let nameFilm1 = prompt ('Один из последних просмотренных фильмов?', ''),
+    raitFilm1 = +prompt ('На сколько оцените его?', ''),
+    nameFilm2 = prompt ('Один из последних просмотренных фильмов?', ''),
+    raitFilm2 = +prompt ('На сколько оцените его?', '');
+
+    personalMovieDB.movies[nameFilm1] = raitFilm1;
+    personalMovieDB.movies[nameFilm2] = raitFilm2; 
+    console.log(personalMovieDB);
+    
 
 
-var money = +prompt("Ваш буджет на месяц?", ""),
+/*let money = +prompt("Ваш буджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", ""),
     quastion1 = prompt("Введите обязательную статью расходов в этом месяце:", ""),
     quastion2 = +prompt("Во сколько обойдется?", ""),
     quastion3 = prompt("Введите обязательную статью расходов в этом месяце:", ""),
     quastion4 = +prompt("Во сколько обойдется?", "");
 
-var appData = {
+let appData = {
     budget: money,
-    timeData: [time],
+    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
@@ -20,8 +40,9 @@ var appData = {
 appData.expenses[quastion1] = quastion2;
 appData.expenses[quastion3] = quastion4;
 
-var budgetDay = appData.budget/30;
+let budgetDay = appData.budget/30;
 alert ("Ваш дневной бюджет составит:" + budgetDay);
 
-var budgetDayFree = (appData.budget-(quastion2+quastion4))/30;
+let budgetDayFree = (appData.budget-(quastion2+quastion4))/30;
 alert ("Ваш дневной бюджет с учетом обязательных платежей составит:" + budgetDayFree);
+*/
